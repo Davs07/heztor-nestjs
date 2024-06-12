3236046ef5b03a49f1bb27680d5b74f89c2df091
Davs07 <botsymeisterspieler@gmail.com>
2024-06-12 18:52:52 -0500

    "Added CORS configuration to main.ts and an empty line to schema.prisma"

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,10 @@ import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:3001', // Cambia esto al dominio que desees permitir
+    credentials: true, // Para permitir cookies
+  });
   app.use(cookieParser())
   await app.listen(3000);
 }
